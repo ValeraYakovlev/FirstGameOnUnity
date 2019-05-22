2e437a5768b698b098aa948122e7d15ca37e1ea1
ValeraYakovlev <49609854+ValeraYakovlev@users.noreply.github.com>
2019-05-22 22:25:42 +0300

    Summary

---
+++ Game/Assets/Sun_Temple/Scripts/FPSInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSInput : MonoBehaviour
+{
+    public float speed = 0.12f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        transform.Translate(deltaX, 0, deltaZ);
+    }
+
+}
---
+++ Game/Assets/Sun_Temple/Scripts/MouseLook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float sensitivityHor = 5.0f; 
+    public float sensitivityVert = 5.0f; 
+    public float minimumVert = -45.0f; 
+    public float maximumVert = 45.0f;
+    private float _rotationX = 0; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+        _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+        float delta = Input.GetAxis("Mouse X") * sensitivityHor; 
+        float rotationY = transform.localEulerAngles.y + delta;
+        transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+    }
+}
